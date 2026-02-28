87ffecd2fbc455940b77c0b17bcd96c0505c7a84
LUCKMAN WORLD <199333163+xpertforextradeinc@users.noreply.github.com>
2026-02-28 01:21:07 -0500

    Create vault.js

---
+++ pages/vault.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout'
+
+export default function Vault() {
+  return (
+    <Layout>
+      <div className="container">
+        <h1>Founder Vault</h1>
+        <p>Limited access vault launching soon.</p>
+      </div>
+    </Layout>
+  )
+}
