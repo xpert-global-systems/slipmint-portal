494a6dd58cb39edb28c735cae0ff61046730228c
LUCKMAN WORLD <199333163+xpertforextradeinc@users.noreply.github.com>
2026-02-28 01:18:03 -0500

    Create NewsletterForm.js

---
+++ components/NewsletterForm.js
@@ -0,0 +1,9 @@
+export default function NewsletterForm() {
+  return (
+    <div className="container">
+      <h3>Join Weekly Market Brief</h3>
+      <input type="email" placeholder="Enter your email" />
+      <button>Subscribe</button>
+    </div>
+  )
+}
