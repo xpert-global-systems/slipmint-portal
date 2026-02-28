2435d01119d7c1cdf9c1514c37c411a454a3769e
LUCKMAN WORLD <199333163+xpertforextradeinc@users.noreply.github.com>
2026-02-28 01:18:37 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout'
+import Hero from '../components/Hero'
+import NewsletterForm from '../components/NewsletterForm'
+
+export default function Home() {
+  return (
+    <Layout>
+      <Hero />
+      <NewsletterForm />
+    </Layout>
+  )
+}
