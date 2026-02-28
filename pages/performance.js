44dbeaa3f69f7b204fbd17314913cddd90d5d285
LUCKMAN WORLD <199333163+xpertforextradeinc@users.noreply.github.com>
2026-02-28 01:19:46 -0500

    Create performance.js

---
+++ pages/performance.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout'
+
+export default function Performance() {
+  return (
+    <Layout>
+      <div className="container">
+        <h1>Performance Reports</h1>
+        <p>Weekly structured breakdowns will appear here.</p>
+      </div>
+    </Layout>
+  )
+}
