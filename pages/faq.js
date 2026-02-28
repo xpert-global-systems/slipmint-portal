49f8cc81052447b8aa558e1b86b782ae8b1de05f
LUCKMAN WORLD <199333163+xpertforextradeinc@users.noreply.github.com>
2026-02-28 01:21:44 -0500

    Create faq.js

---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout'
+
+export default function FAQ() {
+  return (
+    <Layout>
+      <div className="container">
+        <h1>FAQ</h1>
+        <p>Transparency and structure matter here.</p>
+      </div>
+    </Layout>
+  )
+}
