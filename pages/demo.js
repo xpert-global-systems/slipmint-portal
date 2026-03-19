3e10dec6738f34e2258ce48ec129acba1239cfdf
LUCKMAN WORLD <199333163+xpertforextradeinc@users.noreply.github.com>
2026-03-19 16:47:09 +0100

    Add Demo component for dashboard preview

---
+++ pages/demo.js
@@ -0,0 +1,14 @@
+export default function Demo() {
+  return (
+    <div>
+      <h1>Dashboard Preview</h1>
+      <p>Balance: $12,450</p>
+
+      <h3>Recent Activity</h3>
+      <ul>
+        <li>+ $500 deposit</li>
+        <li>- $200 transfer</li>
+      </ul>
+    </div>
+  );
+}
