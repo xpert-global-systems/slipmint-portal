dc4b91871ff78308b3149832a2e893c15e1a3469
LUCKMAN WORLD <199333163+xpertforextradeinc@users.noreply.github.com>
2026-03-19 16:52:58 +0100

    Add demo and signup buttons to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
 import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import NewsletterForm from '../components/NewsletterForm'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
+
+      {/* Core Actions */}
+      <div style={{ textAlign: 'center', marginTop: '20px' }}>
+        <Link href="/demo">
+          <button style={{ marginRight: '10px' }}>View Demo</button>
+        </Link>
+
+        <Link href="/signup">
+          <button>Create Account</button>
+        </Link>
+      </div>
+
+      {/* Optional */}
       <NewsletterForm />
     </Layout>
   )
