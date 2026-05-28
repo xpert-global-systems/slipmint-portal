8de2885731a7f99b74bbc4f9acd5b17d058f0e23
LUCKMAN WORLD <199333163+xpertforextradeinc@users.noreply.github.com>
2026-05-28 01:38:19 +0100

    Update env-test.js

--- pages/api/env-test.js
+++ pages/api/env-test.js
@@ -1,5 +1,9 @@
 export default function handler(req, res) {
   res.json({
-    test: process.env.NEXT_PUBLIC_TEST_VALUE || "Not found"
+    test: process.env.NEXT_PUBLIC_TEST_VALUE || "Not found",
+    apiKey: process.env.FIREBASE_API_KEY ? "Loaded" : "Missing",
+    projectId: process.env.FIREBASE_PROJECT_ID ? "Loaded" : "Missing",
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN ? "Loaded" : "Missing"
   });
 }
+
