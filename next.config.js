d44da2b556e2a783c507bc1c6c66f212434b60a1
LUCKMAN WORLD <199333163+xpertforextradeinc@users.noreply.github.com>
2026-06-01 18:03:19 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Explicitly disable App Router to prevent conflict with Pages Router
-  experimental: {
-    appDir: false,
-  },
-  // Webpack config to exclude app directory from build
-  webpack: (config, { isServer }) => {
-    config.watchOptions = {
-      ...config.watchOptions,
-      ignored: ['**/app/**'],
+
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": __dirname,
     };
     return config;
   },
-  // Turbopack config to exclude app directory
-  turbopack: {
-    resolveAlias: {
-      '@': './pages',
-    },
-  },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
