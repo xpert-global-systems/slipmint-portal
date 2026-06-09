b1505c5c8d266fc8c6d5ac761bfa484b4b13ff31
LUCKMAN WORLD <199333163+xpertforextradeinc@users.noreply.github.com>
2026-06-09 05:57:20 +0100

    Update env-test.js

--- pages/api/env-test.js
+++ pages/api/env-test.js
@@ -106,3 +106,8 @@ export default async function handler(req, res) {
     });
   }
 }
+
+console.log("API KEY:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+console.log("AUTH DOMAIN:", process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN);
+console.log("PROJECT ID:", process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID);
+console.log("SCRAPELLM:", process.env.SCRAPELLM_API_KEY);
