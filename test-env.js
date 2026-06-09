8305a4e22db4dfb90bbc90e24ea8e684380c803e
LUCKMAN WORLD <199333163+xpertforextradeinc@users.noreply.github.com>
2026-06-09 05:56:35 +0100

    Delete test-env.js

--- test-env.js
+++
@@ -1,4 +0,0 @@
-console.log("API KEY:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
-console.log("AUTH DOMAIN:", process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN);
-console.log("PROJECT ID:", process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID);
-console.log("SCRAPELLM:", process.env.SCRAPELLM_API_KEY);
