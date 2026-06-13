b1965d9893d0bfadd39680ca16b828e78f13573f
LUCKMAN WORLD <199333163+xpertforextradeinc@users.noreply.github.com>
2026-06-13 22:39:35 +0100

    Update firebase-test.js

--- src/pages/api/firebase-test.js
+++ src/pages/api/firebase-test.js
@@ -6,13 +6,19 @@ export default function handler(req, res) {
 
     return res.status(200).json({
       success: true,
-      projectId: app.options.projectId || "NO_PROJECT_ID",
+      projectId: app.options.projectId || null,
+
+      envs: {
+        FIREBASE_SERVICE_ACCOUNT: !!process.env.FIREBASE_SERVICE_ACCOUNT,
+        FIREBASE_PROJECT_ID: !!process.env.FIREBASE_PROJECT_ID,
+        FIREBASE_CLIENT_EMAIL: !!process.env.FIREBASE_CLIENT_EMAIL,
+        FIREBASE_PRIVATE_KEY: !!process.env.FIREBASE_PRIVATE_KEY,
+      },
     });
   } catch (error) {
     return res.status(500).json({
       success: false,
       error: error.message,
-      stack: error.stack,
     });
   }
 }
