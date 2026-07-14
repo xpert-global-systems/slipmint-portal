c5ec6cfe23b9064c8cf19100e8e99ff29129b0d1
LUCKMAN WORLD <199333163+xpertforextradeinc@users.noreply.github.com>
2026-07-14 05:03:18 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import '../styles/globals.css'
 import ChatBubble from '../components/ChatBubble'
+import Script from 'next/script'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        id="adsense"
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7755614390195735"
+        crossOrigin="anonymous"
+      />
+
       <Component {...pageProps} />
       <ChatBubble />
     </>
